f7f6c72a0feec97d77446d8b4ba9f0c907ced3b3
vishymails <vishymails@gmail.com>
2022-01-27 14:34:12 +0530

    flake 8 in tox.ini has been  updated

--- tests/test_config.py
+++ tests/test_config.py
@@ -1,4 +1,18 @@
+import pytest
+
 def test_generic() :
     a = 30
     b =30
     assert a == b
+
+
+class NotInRange (Exception) :
+    def __init__(self, message="value not in range") :
+        self.message = message 
+        super().__init__(self.message)
+
+def test_generic1() :
+    a = 5
+    with pytest.raises(NotInRange) :
+        if a not in range(10, 20) :
+            raise NotInRange
